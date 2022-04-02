7241693e422f15e02346967f16d16a1c5c086c45
VS-Lucas <ufpelucasv@gmail.com>
2022-04-01 21:16:54 -0300

    correção

--- parts_made/mux_ALUOut.v
+++ parts_made/mux_ALUOut.v
@@ -2,6 +2,7 @@
     MUX_ALUOUT seleciona saída:
     - PC + 4
     - ALUOut
+    - sinal extendido 26-28 + 31-28 PC
     - EPC
     - Sinal extendido 16-32
     - Mux que dá entrada na memória 
@@ -19,12 +20,12 @@ module mux_ALUOut(
 
     always @(data_0, or data_1 or ext26_to_28 or EPCOut or sign_ext_out or mux_mem_out or selector) begin
         case(selector)
-            3'b000 : data_out = data_0
-            3'b001 : data_out = data_1
-            3'b010 : data_out = ext26_to_28
-            3'b011 : data_out = EPCOut
-            3'b100 : data_out = sign_ext_out
-            3'b101 : data_out = mux_mem_out
+            3'b000 : data_out = data_0;
+            3'b001 : data_out = data_1;
+            3'b010 : data_out = ext26_to_28;
+            3'b011 : data_out = EPCOut;
+            3'b100 : data_out = sign_ext_out;
+            3'b101 : data_out = mux_mem_out;
         endcase
     end
 
