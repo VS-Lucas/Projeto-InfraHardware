13576b4a4ddc422a0dc5dd163ceb0dda2c30fd1a
Severino Carlos <63283908+severinocarlos@users.noreply.github.com>
2022-04-08 20:27:43 -0300

    update sign

--- parts_made/shift2_26_to_28_pc.v
+++ parts_made/shift2_26_to_28_pc.v
@@ -1,11 +1,18 @@
 module shift2_26_to_28_pc (
-    input wire [25:0] data_0,
+    input wire [4:0] rs,
+    input wire [4:0] rt,
+    input wire [15:0] imediato,
     input wire [31:28] pc,
-    output wire [27:0] data_out
+    output wire [31:0] data_out
 );
-    wire [27:0] data_aux;
+    reg [27:0] data_aux;
+    reg [9:0] aux;
+    reg [25:0] aux2;
 
-    assign data_aux = {data_0, 2'b0};
+    assign aux = {rs, rt};
+    assign aux2 = {aux, imediato};
+
+    assign data_aux = {aux2, 2'b0};
     assign data_out = {pc, data_aux};
     
 endmodule
