e73c9f6c67c57323c4972d7876af5f5252c79317
Severino Carlos <63283908+severinocarlos@users.noreply.github.com>
2022-04-08 02:03:49 -0300

    concatena com PC

---
+++ parts_made/shift2_26_to_28_pc.v
@@ -0,0 +1,11 @@
+module shift2_26_to_28_pc (
+    input wire [25:0] data_0,
+    input wire [31:28] pc,
+    output wire [27:0] data_out
+);
+    wire [27:0] data_aux
+
+    assign data_aux = {data_0, 2'b0};
+    assign data_out = {pc, data_aux};
+    
+endmodule
