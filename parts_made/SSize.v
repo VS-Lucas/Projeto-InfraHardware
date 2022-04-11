f7fd5661fe176c925f7f45b9e00d58cb78afdcd3
Severino Carlos <63283908+severinocarlos@users.noreply.github.com>
2022-04-11 15:58:56 -0300

    projeto finalizado

--- parts_made/SSize.v
+++ parts_made/SSize.v
@@ -1,5 +1,5 @@
 module SSize (
-    input wire sel,
+    input wire [1:0] sel,
     input wire [31:0] mem_out,
     input wire [31:0] b_out,
     output reg [31:0] data_out
@@ -7,8 +7,9 @@ module SSize (
     
     always @(*) begin
         case(sel)
-            1'b0: data_out = {mem_out[31:16], b[15:0]};
-            1'b1: data_out = {mem_out[31:8], b_out[7:0]};
+            2'b00: data_out = {mem_out[31:16], b_out[15:0]};
+            2'b01: data_out = {mem_out[31:8], b_out[7:0]};
+            2'b10: data_out = b_out;
         endcase
     end
     
